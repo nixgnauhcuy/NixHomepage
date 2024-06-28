2d8bdbab544ed763b50ca21337b820445f7c82a0
nixgnauhcuy <nixgnauhcuy@gmail.com>
2024-06-28 09:32:11 +0800

    [feat]: Add initial project [scope]: project [note]: none

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('pug', function() {
+  return gulp.src('src/components/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/css/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+  gulp.watch('src/components/**/*.pug', gulp.series('pug'));
+  gulp.watch('src/css/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/js/**/*.js', gulp.series('js'));
+  gulp.watch('src/assets/**/*', gulp.series('images'));
+});
+
+gulp.task('default', gulp.series('clean', gulp.parallel('pug', 'sass', 'js', 'images'), 'watch'));
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('欢迎来到我的个人主页');
+  });

62a9dd0c477ee72e5c3408bf4443fa85a88ca376
nixgnauhcuy <nixgnauhcuy@gmail.com>
2024-07-10 09:43:21 +0800

    [feat]: add webgl-fluid js and add mode switch [scope]: project [note]:   1. Add webgl-fluid js and Adjust and adapt   2. Add light and dark mode switch

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-window['webgl-fluid'].default(document.querySelector('canvas'));
-document.querySelector('main')
-  .addEventListener('mousemove', event => {
-    newEvent = new event.constructor(
-      event.type, event);
-    document.querySelector('canvas')
-      .dispatchEvent(newEvent);
-});

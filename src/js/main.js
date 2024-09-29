370fce4add0dc8450bd96ee0f7ad9a0ad945f6d1
nixgnauhcuy <nixgnauhcuy@gmail.com>
2024-09-29 10:06:08 +0800

    [feat]: add custom font settings [scope]: config.json, main.js [note]: none

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
 const config_fs = /* CONFIG_PLACEHOLDER */;
 const canvas = document.getElementsByTagName('canvas')[0];
+const fontFamily = config_fs.site.font_family.name;
+const fontUrl = config_fs.site.font_family.url;
 
 if (config_fs.config.canvas_gradient) {
   canvas.classList.add('canvas-gradient');
 }
+
+const style = document.createElement('style');
+style.innerHTML = `
+  @font-face {
+    font-family: '${fontFamily}';
+    src: url('${fontUrl}');
+  }
+  body {
+    font-family: '${fontFamily}';
+  }
+`;
+document.head.appendChild(style);
 
 function toggleTheme() {
   document.body.classList.toggle("light-mode");
